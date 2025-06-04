94cdde50ea93ee378189d262012857e8f1cfcbbc
subora.jin <subora.jin@hc-sys.jp>
2025-06-04 12:48:30 +0900

    GET と　POST

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,22 +1,33 @@
 package raisetech.student.management;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	public static void main(String[] args) {
+	@Autowired
+	private  StudentRepository repository;
 
+	private String name = "Enami Kouji";
+
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+   @GetMapping("/name")
+	 public String getName(){
+		return name;
+	 }
 
-	@GetMapping("/hello")
-	public  String hello(){
-		return "Hello,World";
-	}
-	
+	 @PostMapping("/name")
+		public void setName(String name){
+		this.name = name;
+	 }
 }
