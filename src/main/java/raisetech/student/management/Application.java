b6dc9932dee127192ee695f4e542ff27726afaee
Jin5555C <subora.info.2020@gmail.com>
2025-06-25 12:29:54 +0900

    受講生コース情報の一覧をDBから見れるようにする。#4

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -4,11 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -23,28 +19,12 @@ public class Application {
 	}
 
 	@GetMapping("/students")
-	public List<Student> getAllStudents() {
-		return repository.findAllStudents();
+	public List<Student> getStudentList() {
+		return repository.searchStudents();
 	}
 
-	@GetMapping("/student")
-	public String getStudentInfo(@RequestParam String name) {
-		Student student = repository.searchByName(name);
-		return student.getName() + " " + student.getAge() + "歳";
-	}
-
-	@PostMapping("/student")
-	public void registerStudent(String name, int age) {
-		repository.registerStudent(name, age);
-	}
-
-	@PatchMapping("/student")
-	public void updateStudentName(String name, int age) {
-		repository.updateStudent(name, age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name) {
-		repository.deleteStudent(name);
+	@GetMapping("/students_courses")
+	public List<StudentCourse> getStudentCourseList() {
+		return repository.searchStudentCourses();
 	}
 }
