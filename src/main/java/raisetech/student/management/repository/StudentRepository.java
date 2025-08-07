e965ab02423daf29df786b91c89d18c33bf5d1e1
Jin5555C <subora.info.2020@gmail.com>
2025-08-07 21:00:41 +0900

    34_リファクタリング

--- src/main/java/raisetech/student/management/repository/StudentRepository.java
+++ src/main/java/raisetech/student/management/repository/StudentRepository.java
@@ -11,25 +11,44 @@ import raisetech.student.management.data.StudentCourses;
 
 /**
  * 受講生情報を扱うレポジトリ
+ *　受講生テーブルと受講生コース情報テーブルに紐づくRepositoryです。
  *
- * 全件検索、単一検索によりコース情報の検索が行えるレポジトリです。
  */
 @Mapper
 public interface StudentRepository {
 
   /**
+   *受講生の全件検索を行います。
    *
-   * @return 全件検索した受講生情報の一覧
+   * @return 受講生一覧（全件）
    */
   @Select("SELECT * FROM students")
   List<Student> search();
 
+  /**
+   *受講生の検索を行います。
+   *
+   * @param id 受講生ID
+   * @return 受講生
+   */
   @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(Integer id);
 
+  /**
+   *受講生のコース情報の全件検索を行います。
+   *
+   * @return 受講生のコース情報一覧（全件）
+   */
   @Select("SELECT * FROM students_courses")
   List<StudentCourses> searchStudentsCoursesList();
 
+
+  /**
+   *受講生IDに紐づく受講生コース情報を検索します。
+   *
+   * @param student_id 受講生ID
+   * @return 受講生IDに紐づく受講コース情報
+   */
   @Select("SELECT * FROM students_courses WHERE student_id = #{student_id}")
   List<StudentCourses> searchStudentsCourses(Integer student_id);
 
--- src/main/java/raisetech/student/management/service/StudentService.java
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -5,39 +5,55 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourses;
 import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.repository.StudentRepository;
 
+/*
+* 受講生情報を取り扱うサービスです。
+* 受講生の検索や登録・更新処理を行います。
+* */
 @Service
 public class StudentService {
   private StudentRepository repository;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentService(StudentRepository repository) {
+  public StudentService(StudentRepository repository, StudentConverter converter) {
+
     this.repository = repository;
+    this.converter = converter;
   }
 
-  public List<Student> searchStudentList() {
-    return repository.search();
+  /*受講生一覧検索。
+   * 全件検索を行うので、条件指定は行いません。
+   *
+   * ＠return 受講生一覧（全件）
+   * */
+  public List<StudentDetail> searchStudentList() {
+    List<Student> studentList = repository.search();
+    List<StudentCourses> studentCoursesList = repository.searchStudentsCoursesList();
+    return converter.convertStudentDetails(studentList, studentCoursesList);
   }
 
+
+/*
+* 受講生検索です。
+* IDに紐づく受講生情報を取得した後、その受講生に紐づく受講生コースの情報を取得して設定します。
+*
+* @param id 受講生ID
+* @return 受講生
+* */
   public StudentDetail searchStudent(Integer id) {
     Student student = repository.searchStudent(id);
     List<StudentCourses> studentsCourses = repository.searchStudentsCourses(student.getId());
-    StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudent(student);
-    studentDetail.setStudentCourses(studentsCourses);
-    return studentDetail;
-  }
-
-  public List<StudentCourses> searchStudentCoursesList() {
-    return repository.searchStudentsCoursesList();
+    return new StudentDetail(student, studentsCourses);
   }
 
   @Transactional
-  public void registerStudent(StudentDetail studentDetail){
+  public StudentDetail registerStudent(StudentDetail studentDetail){
     repository.registerStudent(studentDetail.getStudent());
     for (StudentCourses studentsCourse : studentDetail.getStudentCourses()){
       studentsCourse.setStudentId(studentDetail.getStudent().getId());
@@ -45,6 +61,7 @@ public class StudentService {
       studentsCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
       repository.registerStudentCourse(studentsCourse);
     }
+    return studentDetail;
   }
 
   @Transactional
