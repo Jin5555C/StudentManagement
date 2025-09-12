f5dac4f11b89697035aac3528174ad9aee1a5b45
Jin5555C <subora.info.2020@gmail.com>
2025-09-12 13:59:24 +0900

    テストの修正 1．テストメソッド名をlowerCamelCaseに変更 2．StudentDetailはMockから実インスタンスを使うように変更 ３．アサーションをAssertJ に統一 ４．不要なimportは削除 ５．verifyNoMoreInteractionsは削除

--- src/test/java/raisetech/student/management/service/StudentServiceTest.java
+++ src/test/java/raisetech/student/management/service/StudentServiceTest.java
@@ -1,24 +1,20 @@
 package raisetech.student.management.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
@@ -42,8 +38,9 @@ class StudentServiceTest {
     sut = new StudentService(repository,converter);
 
   }
+//  void 受講生詳細の一覧検索_リポジトリとコンバーターの処理が適切に呼び出せていること()
   @Test
-  void 受講生詳細の一覧検索_リポジトリとコンバーターの処理が適切に呼び出せていること(){
+  void searchStudentDetails_shouldCallRepositoryAndConverterProperly() {
     List<Student> studentList = new ArrayList<>();
     List<StudentCourse> studentCourseList = new ArrayList<>();
     when(repository.search()).thenReturn(studentList);
@@ -53,11 +50,12 @@ class StudentServiceTest {
 
     verify(repository, times(1)).search();
     verify(repository, times(1)).searchStudentCourseList();
-    verify(converter, times(1)).convertStudentDetails(studentList,studentCourseList);
+    verify(converter, times(1)).convertStudentDetails(studentList, studentCourseList);
   }
 
   @Test
-  void 受講生詳細の１件検索_単一の受講生Idに紐づく受講生情報とコース情報がレポジトリから適切に呼び出せていること(){
+//void 受講生詳細の１件検索_単一の受講生Idに紐づく受講生情報とコース情報がレポジトリから適切に呼び出せていること()
+  void findStudentDetailById_shouldRetrieveStudentAndCourseFromRepository() {
     Integer studentId = 1;
     Student student = new Student();
     student.setId(studentId);
@@ -71,14 +69,18 @@ class StudentServiceTest {
 
     verify(repository, times(1)).searchStudent(studentId);
     verify(repository, times(1)).searchStudentCourse(studentId);
-  
-    Assertions.assertEquals(student, actual.getStudent(),"受講生情報が一致していること");
-    Assertions.assertEquals(studentCourseList, actual.getStudentCourseList(),"コース情報が一致していること");
 
+    assertThat(actual.getStudent())
+        .as("受講生情報が一致していること")
+        .isEqualTo(student);
+    assertThat(actual.getStudentCourseList())
+        .as("コース情報が一致していること")
+        .isEqualTo(studentCourseList);
   }
 
+//  void 受講生詳細検索_studentがnullの場合はnullを返すこと()
   @Test
-  void 受講生詳細検索_studentがnullの場合はnullを返すこと() {
+  void searchStudentDetails_shouldReturnNullWhenStudentIsNull() {
     Integer studentId = 1;
     when(repository.searchStudent(studentId)).thenReturn(null);
 
@@ -87,22 +89,19 @@ class StudentServiceTest {
     verify(repository, times(1)).searchStudent(studentId);
     verify(repository, never()).searchStudentCourse(anyInt());
 
-    assertNull(actual);
+    assertThat(actual).isNull();
   }
 
   @Test
-  void 受講生詳細登録_受講生と受講生コース情報を別々のテーブルに登録できること(){
+//  void 受講生詳細登録_受講生と受講生コース情報を別々のテーブルに登録できること()
+  void registerStudentDetail_shouldInsertStudentAndCourseSeparately() {
     Student student = new Student();
     StudentCourse course1 = new StudentCourse();
     StudentCourse course2 = new StudentCourse();
 
     List<StudentCourse> courseList = Arrays.asList(course1, course2);
 
-    StudentDetail studentDetail = mock(StudentDetail.class);
-
-    when(studentDetail.getStudent()).thenReturn(student);
-    when(studentDetail.getStudentCourseList()).thenReturn(courseList);
-
+    StudentDetail studentDetail = new StudentDetail(student, courseList);
     StudentDetail result = sut.registerStudent(studentDetail);
 
     verify(repository).registerStudent(student);
@@ -113,8 +112,8 @@ class StudentServiceTest {
   }
 
   @Test
-  void 受講生詳細の更新ができる_コースも更新() {
-    // --- 準備 ---
+//  void 受講生詳細の更新ができる_コースも更新()
+  void updateStudentDetail_shouldUpdateCourse() {
     Student student = new Student();
 
     StudentCourse newCourse = new StudentCourse(); // getId() == null → 新規追加
@@ -132,12 +131,11 @@ class StudentServiceTest {
     verify(repository).updateStudent(student);
     verify(repository).registerStudentCourse(newCourse);
     verify(repository).updateStudentCourse(existingCourse);
-
-    verifyNoMoreInteractions(repository);
   }
 
   @Test
-  void 受講生詳細の更新ができる_コースは新規登録() {
+//  void 受講生詳細の更新ができる_コースは新規登録()
+  void updateStudentDetail_shouldRegisterNewCourse() {
     Student student = new Student();
 
     StudentDetail studentDetail = mock(StudentDetail.class);
@@ -147,6 +145,5 @@ class StudentServiceTest {
     sut.updateStudent(studentDetail);
 
     verify(repository).updateStudent(student);
-    verifyNoMoreInteractions(repository);
   }
 }
