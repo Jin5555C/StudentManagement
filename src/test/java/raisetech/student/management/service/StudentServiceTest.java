b671ecb6eef5aaec353c81ef0a54fccdfaf3d943
Jin5555C <subora.info.2020@gmail.com>
2025-09-15 10:30:49 +0900

    テストの修正 1．StudentDetailはMockから実インスタンスを使うように変更. 2．Factory methodとして、外部ファイルに切り出し ３．アサーションをAssertJ に統一 ４．不要なimportは削除 ５．verifyNoMoreInteractionsはユニットテストのみで、スモークテストは不要のため削除 6．EOF newlineをファイルの末尾に追加

--- src/test/java/raisetech/student/management/service/StudentServiceTest.java
+++ src/test/java/raisetech/student/management/service/StudentServiceTest.java
@@ -1,15 +1,16 @@
 package raisetech.student.management.service;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static util.TestDataFactory.createCourse;
+import static util.TestDataFactory.createStudent;
+import static util.TestDataFactory.createStudentDetail;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -57,8 +58,7 @@ class StudentServiceTest {
 //void 受講生詳細の１件検索_単一の受講生Idに紐づく受講生情報とコース情報がレポジトリから適切に呼び出せていること()
   void findStudentDetailById_shouldRetrieveStudentAndCourseFromRepository() {
     Integer studentId = 1;
-    Student student = new Student();
-    student.setId(studentId);
+    Student student = createStudent(studentId);
 
     List<StudentCourse> studentCourseList = new ArrayList<>();
 
@@ -95,13 +95,11 @@ class StudentServiceTest {
   @Test
 //  void 受講生詳細登録_受講生と受講生コース情報を別々のテーブルに登録できること()
   void registerStudentDetail_shouldInsertStudentAndCourseSeparately() {
-    Student student = new Student();
-    StudentCourse course1 = new StudentCourse();
-    StudentCourse course2 = new StudentCourse();
+    Student student = createStudent(1);
+    StudentCourse course1 = createCourse(null);
+    StudentCourse course2 = createCourse(null);
 
-    List<StudentCourse> courseList = Arrays.asList(course1, course2);
-
-    StudentDetail studentDetail = new StudentDetail(student, courseList);
+    StudentDetail studentDetail = createStudentDetail(student, course1,course2);
     StudentDetail result = sut.registerStudent(studentDetail);
 
     verify(repository).registerStudent(student);
@@ -114,18 +112,12 @@ class StudentServiceTest {
   @Test
 //  void 受講生詳細の更新ができる_コースも更新()
   void updateStudentDetail_shouldUpdateCourse() {
-    Student student = new Student();
+    Student student = createStudent(1);
 
-    StudentCourse newCourse = new StudentCourse(); // getId() == null → 新規追加
-    StudentCourse existingCourse = new StudentCourse();
-    existingCourse.setId(100); // getId() != null → 既存更新
+    StudentCourse newCourse = createCourse(null); //  新規追加
+    StudentCourse existingCourse = createCourse(100); // 既存更新
 
-    List<StudentCourse> courseList = Arrays.asList(newCourse, existingCourse);
-
-    StudentDetail studentDetail = mock(StudentDetail.class);
-    when(studentDetail.getStudent()).thenReturn(student);
-    when(studentDetail.getStudentCourseList()).thenReturn(courseList);
-
+    StudentDetail studentDetail = createStudentDetail(student,newCourse,existingCourse);
     sut.updateStudent(studentDetail);
 
     verify(repository).updateStudent(student);
@@ -136,11 +128,9 @@ class StudentServiceTest {
   @Test
 //  void 受講生詳細の更新ができる_コースは新規登録()
   void updateStudentDetail_shouldRegisterNewCourse() {
-    Student student = new Student();
+    Student student = createStudent(1);
 
-    StudentDetail studentDetail = mock(StudentDetail.class);
-    when(studentDetail.getStudent()).thenReturn(student);
-    when(studentDetail.getStudentCourseList()).thenReturn(null);
+    StudentDetail studentDetail = createStudentDetail(student,(StudentCourse[]) null);
 
     sut.updateStudent(studentDetail);
 
